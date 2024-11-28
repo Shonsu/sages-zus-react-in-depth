22e1b8b60f24a509b66d80f048a0fe9d7c9d4bcf
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-28 15:11:43 +0100

    Exercise: Make Details use mock data + colors

--- vite-project/src/playlists/containers/PlaylistDetails.tsx
+++ vite-project/src/playlists/containers/PlaylistDetails.tsx
@@ -1,15 +1,39 @@
-
-
 // tsrafce
 
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const PlaylistDetails = (props: Props) => {
-  return (
-    <div>PlaylistDetails</div>
-  )
-}
+  const playlist = {
+    id: 123,
+    name: "Playlist 123",
+    public: true,
+    description: "Best playlist",
+  };
 
-export default PlaylistDetails
+  return (
+    <div>
+      {true} {false} {null} {undefined}
+      {/* .grid.gap-5>.grid.gap-2*3>strong{Name}+div{Playlist $$$} */}
+      <div className="grid gap-5">
+        <div className="grid gap-2">
+          <strong>Name</strong>
+          <div>Playlist 001</div>
+        </div>
+
+        <div className="grid gap-2">
+          <strong>Public</strong>
+          <div style={{ color: "red" }}>Yes</div>
+        </div>
+
+        <div className="grid gap-2">
+          <strong>Description</strong>
+          <div>Awesome Playlist</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistDetails;
