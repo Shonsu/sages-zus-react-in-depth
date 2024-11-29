beb81379b402e34cede900cafba6ed56453dda23
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-29 09:49:15 +0100

    Selecting playlists

--- vite-project/src/playlists/containers/PlaylistList.tsx
+++ vite-project/src/playlists/containers/PlaylistList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
@@ -24,18 +24,25 @@ const playlists = [
 ];
 
 const PlaylistList = (props: Props) => {
-  const selectedId = "234";
+  const [selectedId, setSelectedId] = useState(234);
 
-  const selectById = (id: string) => [
-    // /??
-  ];
+  const selectById = (id: number) => {
+    setSelectedId(id);
+  };
 
   return (
     <div>
       {/* .divide-y.divide-slate-400.divide-solid>div*3.px-2.py-5{$. Playlist $$$} */}
+
       <div className="divide-y divide-slate-400 divide-solid">
         {playlists.map((p, index, all) => (
-          <div className="px-2 py-5 bg-primary-300 text-white " key={p.id}>
+          <div
+            className={`px-2 py-5 ${
+              selectedId == p.id ? "bg-primary-300 text-white" : ""
+            } `}
+            onClick={() => setSelectedId(p.id)}
+            key={p.id}
+          >
             {index + 1} {p.name}
           </div>
         ))}
