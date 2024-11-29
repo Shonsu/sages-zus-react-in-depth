ea06afa98ba67a4c2b8931039393f99fd1ee6d58
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-29 10:53:36 +0100

    Fix mock type

--- vite-project/src/playlists/containers/mockPlaylists.tsx
+++ vite-project/src/playlists/containers/mockPlaylists.tsx
@@ -12,19 +12,19 @@ type PlaylistWithTracks = Playlist & {
 
 export const mockPlaylists: Playlist[] = [
   {
-    id: 123,
+    id: '123',
     name: "Playlist 123",
     public: true,
     description: "Awesome playlist",
   },
   {
-    id: 234,
+    id: '234',
     name: "Playlist 234",
     public: false,
     description: "Best playlist",
   },
   {
-    id: 345,
+    id: '345',
     name: "Playlist 345",
     public: true,
     description: "Cool playlist",
