886be2cb1f7e149e928a22a18cdb8c85fd461da9
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-29 09:29:43 +0100

    Exercise: Rendering list of playlists

--- vite-project/src/playlists/containers/PlaylistList.tsx
+++ vite-project/src/playlists/containers/PlaylistList.tsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 type Props = {}
 
+const playlists = [{
+  id: 123,
+  name: "Playlist 123",
+  public: true,
+  description: "Awesome playlist",
+},{
+  id: 234,
+  name: "Playlist 234",
+  public: false,
+  description: "Best playlist",
+},{
+  id: 345,
+  name: "Playlist 345",
+  public: true,
+  description: "Cool playlist",
+}];
+
+
 const PlaylistList = (props: Props) => {
   return (
-    <div>PlaylistList</div>
+    <div>
+      
+      {/* .divide-y.divide-slate-400.divide-solid>div*3.px-2.py-5{$. Playlist $$$} */}
+      <div className="divide-y divide-slate-400 divide-solid">
+        <div className="px-2 py-5">1. Playlist 001</div>
+        <div className="px-2 py-5">2. Playlist 002</div>
+        <div className="px-2 py-5">3. Playlist 003</div>
+      </div>
+
+    </div>
   )
 }
 
