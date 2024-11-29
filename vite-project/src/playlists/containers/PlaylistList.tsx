8d498da30a425c7593b5b7b0aab392dcff058de8
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-29 10:18:09 +0100

    Exercise: Typing data

--- vite-project/src/playlists/containers/PlaylistList.tsx
+++ vite-project/src/playlists/containers/PlaylistList.tsx
@@ -1,27 +1,7 @@
 import React, { useState } from "react";
+import { mockPlaylists } from "./mockPlaylists";
 
 type Props = {};
-
-const playlists = [
-  {
-    id: 123,
-    name: "Playlist 123",
-    public: true,
-    description: "Awesome playlist",
-  },
-  {
-    id: 234,
-    name: "Playlist 234",
-    public: false,
-    description: "Best playlist",
-  },
-  {
-    id: 345,
-    name: "Playlist 345",
-    public: true,
-    description: "Cool playlist",
-  },
-];
 
 const PlaylistList = (props: Props) => {
   const [selectedId, setSelectedId] = useState(234);
@@ -35,7 +15,7 @@ const PlaylistList = (props: Props) => {
       {/* .divide-y.divide-slate-400.divide-solid>div*3.px-2.py-5{$. Playlist $$$} */}
 
       <div className="divide-y divide-slate-400 divide-solid">
-        {playlists.map((p, index, all) => (
+        {mockPlaylists.map((p, index, all) => (
           <div
             className={`px-2 py-5 ${
               selectedId == p.id ? "bg-primary-300 text-white" : ""
---
+++ vite-project/src/playlists/containers/mockPlaylists.tsx
@@ -0,0 +1,20 @@
+export const mockPlaylists = [
+  {
+    id: 123,
+    name: "Playlist 123",
+    public: true,
+    description: "Awesome playlist",
+  },
+  {
+    id: 234,
+    name: "Playlist 234",
+    public: false,
+    description: "Best playlist",
+  },
+  {
+    id: 345,
+    name: "Playlist 345",
+    public: true,
+    description: "Cool playlist",
+  },
+];
