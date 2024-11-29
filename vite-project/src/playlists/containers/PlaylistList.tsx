2556b76bdd91b0b6e791a401195c063aa668e9f8
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-29 09:35:12 +0100

    Rendering lists

--- vite-project/src/playlists/containers/PlaylistList.tsx
+++ vite-project/src/playlists/containers/PlaylistList.tsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const playlists = [{
-  id: 123,
-  name: "Playlist 123",
-  public: true,
-  description: "Awesome playlist",
-},{
-  id: 234,
-  name: "Playlist 234",
-  public: false,
-  description: "Best playlist",
-},{
-  id: 345,
-  name: "Playlist 345",
-  public: true,
-  description: "Cool playlist",
-}];
-
+const playlists = [
+  {
+    id: 123,
+    name: "Playlist 123",
+    public: true,
+    description: "Awesome playlist",
+  },
+  {
+    id: 234,
+    name: "Playlist 234",
+    public: false,
+    description: "Best playlist",
+  },
+  {
+    id: 345,
+    name: "Playlist 345",
+    public: true,
+    description: "Cool playlist",
+  },
+];
 
 const PlaylistList = (props: Props) => {
   return (
     <div>
-      
       {/* .divide-y.divide-slate-400.divide-solid>div*3.px-2.py-5{$. Playlist $$$} */}
       <div className="divide-y divide-slate-400 divide-solid">
-        <div className="px-2 py-5">1. Playlist 001</div>
-        <div className="px-2 py-5">2. Playlist 002</div>
-        <div className="px-2 py-5">3. Playlist 003</div>
+        {playlists.map((p, index, all) => (
+          <div className="px-2 py-5" key={p.id}>{index+1} {p.name}</div>
+        ))}
       </div>
+    </div> 
+  );
+};
 
-    </div>
-  )
-}
-
-export default PlaylistList
+export default PlaylistList;
