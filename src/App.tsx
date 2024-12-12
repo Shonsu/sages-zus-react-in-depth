84915d6ce49e4c2dd4dc6c9c5140263e615486d6
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-12 09:48:08 +0100

    useMemo vs useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,13 +7,19 @@ import { Button } from "primereact/button";
 import PlaylistsView from "./playlists/components/PlaylistsView";
 import AppButton from "./common/components/AppButton";
 import AlbumSearchView from "./music/containers/AlbumSearchView";
+import { checkLogin, login } from "./common/services/Auth";
 
 function App() {
+  
+  const status = useMemo(() => checkLogin(), []); // before render
+  
+  useEffect(() => checkLogin(), []); // after first render
+
   return (
     <>
       <div className=" bg-green-600 py-4">
         <div className="container py-2 ">
-          <AppButton primary className="float-end">
+          <AppButton primary className="float-end" onClick={login}>
             Login
           </AppButton>
           <h1 className="text-3xl font-bold text-white">MusicApp</h1>
