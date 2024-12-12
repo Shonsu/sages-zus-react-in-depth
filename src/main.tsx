5562269157ba4413e158be0b31473cfa01337d90
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-12 12:16:37 +0100

    useQuery - cache, deduplication, retry, reconnect, refocus tab, ...

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,26 @@ import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 
 window.React = React;
 window.ReactDOM = ReactDOM;
-
+import { HTTPError } from "ky";
 import "./index.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const RETRY_CODES = [408, 429, 500, 502, 503, 504];
+
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry(failureCount, error) {
+        if (!(error instanceof HTTPError)) throw "not HTTPError";
+
+        return RETRY_CODES.indexOf(error.response.status) !== -1;
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
