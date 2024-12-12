52ccdf8b3efe005ee697832ef17bd23362fdce2b
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-12 13:42:03 +0100

    ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -60,17 +60,15 @@ const router = createBrowserRouter([
         path: "playlists",
         element: <PlaylistsView />,
       },
-      {
-        path: "*",
-        element: (
-          <div className="text-center">
-            <h1 className=" text-6xl">404</h1>
-            <p>Page not found</p>
-            <a href="/">go to Home</a>
-          </div>
-        ),
-      },
     ],
+    // ErrorBoundary : <errorboundary/>,
+    errorElement: (
+      <div className="my-10 text-center">
+        <h1 className=" text-6xl">404</h1>
+        <p>Page not found</p>
+        <a href="/">go to Home</a>
+      </div>
+    ),
   },
 ]);
 
