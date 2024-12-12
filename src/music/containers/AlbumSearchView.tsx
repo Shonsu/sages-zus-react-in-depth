e9bfbc631eb3469c8243a968a4a5ad6c68d52751
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-12 11:35:42 +0100

    Generic Custom Hook - const { data: results = [], isLoading, error } = useFetchAlbums(query);

--- src/music/containers/AlbumSearchView.tsx
+++ src/music/containers/AlbumSearchView.tsx
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import SearchForm from "../components/SearchForm";
 import ResultsGrid from "../components/ResultsGrid";
 import { ProgressSpinner } from "primereact/progressspinner";
-import { useFetchAlbums } from "./useFetchAlbums";
+import { useFetch, useFetchAlbums } from "./useFetchAlbums";
 import { BigSpinner } from "../../common/components/BigSpinner";
 import { ErrorMessage } from "./ErrorMessage";
 import { VStack } from "../../common/components/Stack";
+import { fetchAlbumSearchResults } from "../../common/services/MusicAPI";
 
 const AlbumSearchView = () => {
   const [query, setQuery] = useState("");
 
-  const { data, isLoading, error } = useFetchAlbums(query);
+  // const { data: results = [], isLoading, error } = useFetchAlbums(query);
+  
+  const {
+    data: results = [],
+    isLoading,
+    error,
+  } = useFetch(query, fetchAlbumSearchResults);
 
   return (
     <VStack>
@@ -19,7 +26,7 @@ const AlbumSearchView = () => {
       <VStack gap="sm">
         <BigSpinner show={isLoading} />
         <ErrorMessage error={error} />
-        <ResultsGrid results={data} />
+        <ResultsGrid results={results} />
       </VStack>
     </VStack>
   );
--- src/music/containers/useFetchAlbums.tsx
+++ src/music/containers/useFetchAlbums.tsx
@@ -1,8 +1,52 @@
 import { useState, useEffect } from "react";
 import { Album } from "../../common/model/Album";
-import { fetchAlbumSearchResults } from "../../common/services/MusicAPI";
+import {
+  fetchAlbumById,
+  fetchAlbumSearchResults,
+} from "../../common/services/MusicAPI";
+import { Options } from "ky";
+
+export function useFetch<T, P>(
+  param: P,
+  fetcher: (query: P, init?: Options) => Promise<T>
+) {
+  const [data, setData] = useState<T | undefined>();
+  const [error, setError] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!param) return;
+    const huston = new AbortController();
+
+    setIsLoading(true);
+    setError(undefined);
+    setData(undefined);
+
+    // fetchAlbumSearchResults(query, { signal: huston.signal })
+    fetcher(param, { signal: huston.signal })
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
+
+    return () => huston.abort("Cancel");
+  }, [param]);
+
+  return {
+    data,
+    isLoading,
+    error,
+  };
+}
 
 export function useFetchAlbums(query: string) {
+  return useFetch(query, fetchAlbumSearchResults);
+}
+
+export function useFetchAlbum(id: string) {
+  return useFetch(id, fetchAlbumById);
+}
+
+export function useFetchAlbums2(query: string) {
   const [data = [], setData] = useState<Album[] | undefined>([]);
   const [error, setError] = useState<unknown>();
   const [isLoading, setIsLoading] = useState(false);
