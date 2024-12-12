1782ba4aef8c8d469e595acb2c12155baf6a58d2
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-12 09:12:59 +0100

    Exericse: Use mock api to fetch data into grid when search is clicked

--- src/common/services/MusicAPI.ts
+++ src/common/services/MusicAPI.ts
@@ -1,5 +1,7 @@
-import { mockAlbums } from "../fixtures/mockAlbums"
+import { mockAlbums } from "../fixtures/mockAlbums";
 
-export const fetchAlbumSearchResults = (query = '') => {
-    return Promise.resolve(mockAlbums)
-}
+export const fetchAlbumSearchResults = (query = "") => {
+  console.log("searching " + query);
+
+  return Promise.resolve(mockAlbums);
+};
--- src/music/containers/AlbumSearchView.tsx
+++ src/music/containers/AlbumSearchView.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchForm from "../components/SearchForm";
 import ResultsGrid from "../components/ResultsGrid";
+import { fetchAlbumSearchResults } from "../../common/services/MusicAPI";
 
 type Props = {};
 
 const AlbumSearchView = (props: Props) => {
+  function search(query: string) {
+    fetchAlbumSearchResults(query);
+  }
+
   return (
     <div>
       {/* .grid.gap-5>div*2 */}
